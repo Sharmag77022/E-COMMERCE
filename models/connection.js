665870603a53836784edca5c7f4d1f52957ea19f
Sanjeev <sharmag77022@gmail.com>
2020-08-12 17:50:10 +0530

    database connection

--- models/connection.js
+++ models/connection.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
  module.exports = ()=>{
-    mongoose.connect('mongodb://localhost:27017/ecommerce', {useNewUrlParser: true,useUnifiedTopology: true});
+    mongoose.connect('mongodb+srv://sanjeev77022:sanjeev5@cluster0.kz8yo.mongodb.net/ecommerce?retryWrites=true&w=majority', {useNewUrlParser: true,useUnifiedTopology: true});
     var db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
     console.log('connection to Database is Successful')
     });
  }
+ //mongodb://localhost:27017/ecommerce
